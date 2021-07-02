8e4a08093ba284cfb1e47a2a9c0001f4c7a34e71
Ian Winson <iankwinson@gmail.com>
2021-07-02 11:32:36 -0500

    Commit1

--- main.py
+++ main.py
@@ -1,1 +1,32 @@
+import os
+from pathlib import Path
+from playsound import playsound
+from pygame import mixer
+
+os.chdir(r"A:\\")
+file = Path("A:\\track.txt")
+mixer.init()
+
+while(1):
+    input("Press Enter to Play Sound")
+    if(file.exists()):
+        f=open('track.txt', 'r')
+        f.seek(0)
+        num = f.read(1)
+        a = "C:\\Users\\PROGRAMING\\Documents\\Floppy\\music"
+        c = ".mp3"
+        path = a + str(int(num)) + c 
+        path2 = "music" + str(int(num)) + ".mp3"
+        print("Playing Sample #" + str(int(num)))
+        # playsound(path)
+        mixer.music.load(path)
+        mixer.music.play()
+        input("Playing sound. Press enter to stop")
+        mixer.music.pause() 
+        # playsound(r'C:\Users\PROGRAMING\Documents\Floppy\one.mp3')
+        # print("Sound 1")
+
+
+
+
 print("test")
